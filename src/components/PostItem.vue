4393d9653ac71a84a924155e3530f60ccaf3e004
Takács Balázs <tbalazs.levente@gmail.com>
2021-11-30 16:34:10 +0100

    PostItem.vue létrehozás

---
+++ src/components/PostItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <li>
+      <div v-if="!edit">
+        <h2>{{ title }}</h2>
+        <div>{{ body }}</div>
+        <button @click="Edit">Edit</button>
+      </div>
+      <div v-if="edit">
+          <input type="text" v-model="title"> <br>
+          <input type="text" v-model="body"> <br>
+          <button @click="Save">Save</button>
+      </div>
+  </li>
+</template>
+
+<script>
+export default {
+    props: ['post'],
+    data() {
+        return {
+            title: this.post.title,
+            body: this.post.body,
+            edit: false,
+        }
+    },
+    methods: {
+        Edit() {
+            this.edit = true
+        },
+        Save() {
+            this.edit = false
+            this.$emit('post-item-changed', {
+                original: {
+                    title: this.title,
+                    body: this.body
+                    },
+                new: {
+                    title: this.title,
+                    body: this.body
+                    },
+            })
+        }
+    }
+}
+</script>
